1c3935a3be1bbd6f530279b4c8f80a4e1457daf3
tima ali <timaali43@gmail.com>
2022-10-22 01:58:42 +0300

    Creates our html file

---
+++ StartingJS.js
@@ -0,0 +1,17 @@
+var add = function(number1, number2) {
+    return number1+number2;
+};
+
+
+var number1 = parseInt(prompt("Enter your First number"));
+var number2 = parseInt(prompt("Enter your second number"));
+var result = add(number1, number2);
+alert(result);
+
+
+
+
+
+
+
+
