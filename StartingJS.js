1b55ae1bfb97dc3fb08ba0e4360b19bbcb759f7e
tima ali <timaali43@gmail.com>
2022-10-23 22:54:56 +0300

    modifies yearOfBirth function

--- StartingJS.js
+++ StartingJS.js
@@ -1,12 +1,14 @@
-var Input = function(){
-    var yearOfBirth = parseInt(prompt("please Enter Your Year of Birth"));
-    
-    if (yearOfBirth === NaN || yearOfBirth > 2022 || yearOfBirth.toString().length > 4) {
-        Input();
+var Input = function(message){
+    var yearOfBirth = (prompt(message));
+    console.log(isNaN(yearOfBirth)); //For Debugging
+    console.log(yearOfBirth); //For debugging
+    if (isNaN(yearOfBirth) || parseInt(yearOfBirth)> 2022 || yearOfBirth.length > 4) {
+        
+        Input("Please Enter a Valid Year of Birth e.g 2019");
     }
     }
 
-    Input();
+    Input("please Enter Your Year of Birth");
 
 
 
