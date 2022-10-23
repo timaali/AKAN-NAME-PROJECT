0e6ae208d9f4c6f847769902df1896c2b6ed650a
tima ali <timaali43@gmail.com>
2022-10-23 23:39:39 +0300

    adds Month and Day to the function

--- StartingJS.js
+++ StartingJS.js
@@ -1,15 +1,28 @@
-var Input = function(message){
-    var yearOfBirth = (prompt(message));
-    console.log(isNaN(yearOfBirth)); //For Debugging
-    console.log(yearOfBirth); //For debugging
-    if (isNaN(yearOfBirth) || parseInt(yearOfBirth)> 2022 || yearOfBirth.length > 4) {
-        
-        Input("Please Enter a Valid Year of Birth e.g 2019");
+var Input = function(inputType, message){
+    var userInput = (prompt(message));
+    if (inputType === "Year"){
+        if (isNaN(userInput) || parseInt(userInput)> 2022 || userInput.length > 4) {        
+            userInput = Input(inputType, "Please Enter a Valid Year of Birth e.g 2019");       
+        }
     }
+    else if (inputType === "Month"){
+        if (isNaN(userInput) || parseInt(userInput)> 12 || parseInt(userInput) < 1) {        
+            userInput = Input(inputType, "Please Enter a Valid Month e.g 1 - 12");       
+        }
     }
 
-    Input("please Enter Your Year of Birth");
+    else if (inputType === "Day"){
+        if (isNaN(userInput) || parseInt(userInput)> 31 || parseInt(userInput) < 1) {        
+            userInput = Input(inputType, "Please Enter a Valid Day of Birth e.g 1 - 31");       
+        }
+    }
+    return userInput;
+}
 
+var Year = Input("Year", "please Enter Your Year of Birth");
+var Month = Input("Month", "please Enter Your Month of Birth");
+var Day = Input("Day", "please Enter Your Day of Birth");
+console.log(Year, Month, Day);
 
 
 
