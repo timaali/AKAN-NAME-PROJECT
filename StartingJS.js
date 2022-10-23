b9cdb32f6b7c971869a1d5c6656a6f4fe9e14059
tima ali <timaali43@gmail.com>
2022-10-24 01:25:36 +0300

    Introduces the Leap year calculations

--- StartingJS.js
+++ StartingJS.js
@@ -1,3 +1,5 @@
+var Year, Month, Day = "";
+
 var Input = function(inputType, message){
     var userInput = (prompt(message));
     if (inputType === "Year"){
@@ -15,6 +17,19 @@ var Input = function(inputType, message){
         if (isNaN(userInput) || parseInt(userInput)> 31 || parseInt(userInput) < 1) {        
             userInput = Input(inputType, "Please Enter a Valid Day of Birth e.g 1 - 31");       
         }
+        if (userInput>30 && [4,6,9,11].includes(Month)) {
+            userInput = Input(inputType, "This Month only has 30 days only, please enter a valid day"); 
+        }
+        if (Month === 2) {
+            if (Year % 4 === 0 && userInput > 29){
+                userInput = Input(inputType, "This Month only has 29 days only, please enter a valid day"); 
+            }
+            else if (Year % 4 !== 0 && userInput > 28){
+                userInput = Input(inputType, "This Month only has 28 days only, please enter a valid day"); 
+            }
+
+        }
+        
     }
 
     else if (inputType === "Gender"){
@@ -44,9 +59,9 @@ var akanName = function(Year, Month, Day, Gender){
 }
 
 var generator = function(){
-    var Year = Input("Year", "please Enter Your Year of Birth");
-    var Month = Input("Month", "please Enter Your Month of Birth");
-    var Day = Input("Day", "please Enter Your Day of Birth");
+    Year = Input("Year", "please Enter Your Year of Birth");
+    Month = Input("Month", "please Enter Your Month of Birth");
+    Day = Input("Day", "please Enter Your Day of Birth");
     var Gender = Input("Gender", "please Enter Your Gender F or M");
     console.log(Year, Month, Day, Gender);
     var Name = akanName(Year, Month, Day, Gender);
